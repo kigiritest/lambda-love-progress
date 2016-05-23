4651557951548063802767766b495fd6cd208c5c
kigiritest <clement@payname.fr>
2016-05-24 01:41:18 +0200

    Init progress for ex 00-login.js

---
+++ basic-kigiri-learn/00-login.js
@@ -0,0 +1,14 @@
+/*
+ * Instructions :
+ * - Open your console and call the function git.login like so :
+ *     git.login('username', 'password')
+ *
+ * - To load next exercise, call the function next.
+ *
+ * Once you are login, your credentials are stored in the localStorage.
+ *
+ * To be forgoten simply clear the localStorage :
+ * -> localStorage.clear()
+ *
+ */
+ 
